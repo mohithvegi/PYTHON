7e73e3f37ab6b072af30eb52ff4120eb3060a15f
Mohith Vegi <mohithvegi@gmail.com>
2021-05-09 00:25:35 +0530

    Delete cricket.py

--- Libraries/CricketTop/cricket.py
+++
@@ -1,6 +0,0 @@
-from cricket_tops import cricdata
-
-print(cricdata.topoditeams())
-print("-----------------------------------------------")
-print(cricdata.testtop10batters())
-print("-----------------------------------------------")
