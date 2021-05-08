9fd7099f76eeea2c28f126f916788c2565baf146
mohithvegi <mohithvegi@gmail.com>
2021-05-08 23:55:19 +0530

    Python Libraries

---
+++ Libraries/CricketTop/cricket.py
@@ -0,0 +1,6 @@
+from cricket_tops import cricdata
+
+print(cricdata.topoditeams())
+print("-----------------------------------------------")
+print(cricdata.testtop10batters())
+print("-----------------------------------------------")
